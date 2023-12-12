b99df1b5a1e42e4b6cd041aa2107735128da3f0b
caselab_LJY <wnsdyd1489@gmail.com>
2023-12-12 18:00:11 +0900

    fitst_update!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+
+
 
 function App() {
+  const [isFolderOpen, setFolderOpen] = useState(false);
+
+  const toggleFolder = () => {
+    setFolderOpen(!isFolderOpen);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>Auto Labeling</div>
+      </div>
+      <h3>Open Folder!</h3>
+      {/* 폴더 열기 버튼 */}
+      <button onClick={toggleFolder}>
+        {isFolderOpen ? '폴더 닫기' : '폴더 열기'}
+      </button>
+
+      {/* 폴더 내용 */}
+      {isFolderOpen && (
+        <div>
+          {/* 폴더 내용을 이곳에 추가하세요 */}
+        </div>
+      )}
     </div>
   );
 }
